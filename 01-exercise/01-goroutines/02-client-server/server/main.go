2608a97d1fda39fb5da7afe280c26c154516e8e7
Maksym Yarema <macsym2@gmail.com>
2024-07-30 16:12:11 +0300

    complete exercise-goroutines-client-server

--- 01-exercise/01-goroutines/02-client-server/server/main.go
+++ 01-exercise/01-goroutines/02-client-server/server/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
 )
 
 func main() {
 	// TODO: write server program to handle concurrent client connections.
+	listener, err := net.Listen("tcp", "localhost:8000")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			continue
+		}
+
+		go handleConn(conn)
+	}
 }
 
 // handleConn - utility function
@@ -19,6 +34,8 @@ func handleConn(c net.Conn) {
 		if err != nil {
 			return
 		}
+
+
 		time.Sleep(time.Second)
 	}
 }
